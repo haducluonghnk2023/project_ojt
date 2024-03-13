e1eac7cf6d49ebb92402bce94128d2d5e3e39ad6
haducluonghnk2023 <haducluongdz@gmail.com>
2024-03-13 14:13:32 +0700

    add

--- session 06-project/js/index.js
+++ session 06-project/js/index.js
@@ -1,30 +1,93 @@
-const products = [
-    {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.600.000"},
-    {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.700.000"},
-    {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.800.000"},
-    {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"2.000.000"},
-    {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.200.000"},
-    {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.300.000"},
-    {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.400.000"},
-    {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.500.000"},
-]
-function renderProduct(container, product) {
-    const div = document.createElement('div');
-    div.classList.add('product');
-    div.innerHTML = `
-        <img src="${product.img}" alt="">
-        <p>${product.name}</p>
-        <p>Giá: ${product.price}</p>
-    `;
-    container.appendChild(div);
-}
+// const products = [
+//     {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.600.000"},
+//     {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.700.000"},
+//     {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.800.000"},
+//     {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"2.000.000"},
+//     {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.200.000"},
+//     {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.300.000"},
+//     {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.400.000"},
+//     {img: "./asset/images/chuot 1.jpg",name :"chuột không dây",price:"1.500.000"},
+// ]
+// function renderProduct(container, product) {
+//     const div = document.createElement('div');
+//     div.classList.add('product');
+//     div.innerHTML = `
+//         <img src="${product.img}" alt="">
+//         <p>${product.name}</p>
+//         <p>Giá: ${product.price}</p>
+//     `;
+//     container.appendChild(div);
+// }
 
-const main1Container = document.querySelector('.main1');
-products.slice(0, 4).forEach(product => {
-    renderProduct(main1Container, product);
-});
+// const main1Container = document.querySelector('.main1');
+// products.slice(0, 4).forEach(product => {
+//     renderProduct(main1Container, product);
+// });
 
-const main2Container = document.querySelector('.main2');
-products.slice(4).forEach(product => {
-    renderProduct(main2Container, product);
-});
+// const main2Container = document.querySelector('.main2');
+// products.slice(4).forEach(product => {
+//     renderProduct(main2Container, product);
+// });
+
+// tạo mảng chứa danh sách sản phẩm
+// quản lí product
+
+let products = [
+    {
+        image : "./asset/images/chuot 1.jpg",
+        name : "chuot khong day",
+        price :"1.200.000",
+        id : 1,
+        stock : 20,
+    },
+    {
+        image : "./asset/images/chuot 1.jpg",
+        name : "chuot khong day",
+        price :"1.200.000",
+        id : 1,
+        stock : 20,
+    },
+    {
+        image : "./asset/images/chuot 1.jpg",
+        name : "chuot khong day",
+        price :"1.200.000",
+        id : 1,
+        stock : 20,
+    },
+    {
+        image : "./asset/images/chuot 1.jpg",
+        name : "chuot khong day",
+        price :"1.200.000",
+        id : 1,
+        stock : 20,
+    },
+    {
+        image : "./asset/images/chuot 1.jpg",
+        name : "chuot khong day",
+        price :"1.200.000",
+        id : 1,
+        stock : 20,
+    },
+    {
+        image : "./asset/images/chuot 1.jpg",
+        name : "chuot khong day",
+        price :"1.200.000",
+        id : 1,
+        stock : 20,
+    },
+    {
+        image : "./asset/images/chuot 1.jpg",
+        name : "chuot khong day",
+        price :"1.200.000",
+        id : 1,
+        stock : 20,
+    },
+    {
+        image : "./asset/images/chuot 1.jpg",
+        name : "chuot khong day",
+        price :"1.200.000",
+        id : 1,
+        stock : 20,
+    },
+    
+];
